8290b8d9af845d634177094f5e5c354ccdb3d0a8
Ibrahim Chhaya <16260791+IbrahimChhaya@users.noreply.github.com>
2024-02-14 23:13:53 -0500

    fix

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -5,12 +5,5 @@ input_dat_file = "data/pfgd_test.dat"
 
 print(cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file))
 
-whileLoop = True
-while whileLoop:
-    letter = input("magic letter?")
-    if letter.lower() == 's':
-        whileLoop = False
-
-
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
 #print the resulting data
