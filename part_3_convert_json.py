23d496549cd57349c244817f4c2596e2b2318d67
Ibrahim Chhaya <16260791+IbrahimChhaya@users.noreply.github.com>
2024-02-20 22:32:40 -0500

    dat file does not load in game

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -2,17 +2,16 @@ import cc_dat_utils
 import cc_classes
 import json
 
-#Part 3
-#Load your custom JSON file
-#Convert JSON data to CCLevelPack
-#Save converted data to DAT file
 
-def json_to_cclevelpack( json_data ):
+# Part 3
+# Load your custom JSON file
+# Convert JSON data to CCLevelPack
+# Save converted data to DAT file
 
+def json_to_cclevelpack(json_data):
     cc_level_pack = cc_classes.CCLevelPack()
 
     for level in json_data:
-        
         level_number = level["level_number"]
         time = level["time"]
         num_chips = level["num_chips"]
@@ -23,39 +22,52 @@ def json_to_cclevelpack( json_data ):
         cc_trap_controls_field = level["optional_fields"]["CCTrapControlsField"]
         cc_cloning_machine_controls_field = level["optional_fields"]["CCCloningMachineControlsField"]
         upper_layer = level["upper_layer"]
-        
+        #lower_layer = level["lower_layer"]
+
         cc_level = cc_classes.CCLevel()
         cc_level.level_number = level_number
         cc_level.time = time
         cc_level.num_chips = num_chips
         cc_level.upper_layer = upper_layer
+        #cc_level.lower_layer = lower_layer
         
+
         cc_level.add_field(cc_classes.CCMapTitleField(cc_map_title_field))
         cc_level.add_field(cc_classes.CCMapHintField(cc_map_hint_field))
         cc_level.add_field(cc_classes.CCEncodedPasswordField(cc_encoded_password_field))
-        
+
         # if more than one monster, put this in a for loop and instantiate each monster
         monster_coords = cc_classes.CCCoordinate(cc_monster_movement_field["x"], cc_monster_movement_field["y"])
-        cc_level.add_field(cc_classes.CCMonsterMovementField(monster_coords))
+        monster_list = [monster_coords]
+        cc_level.add_field(cc_classes.CCMonsterMovementField(monster_list))
 
         # same with traps and cloning machine
         trap_control_b_coord = cc_classes.CCCoordinate(cc_trap_controls_field["bx"], cc_trap_controls_field["by"])
         trap_control_t_coord = cc_classes.CCCoordinate(cc_trap_controls_field["tx"], cc_trap_controls_field["ty"])
-        trap_control = cc_classes.CCTrapControl(trap_control_b_coord, trap_control_t_coord)
-        cc_level.add_field(cc_classes.CCTrapControlsField(trap_control))
+        trap_control = cc_classes.CCTrapControl(trap_control_b_coord.x, trap_control_b_coord.y, trap_control_t_coord.x,
+                                                trap_control_t_coord.y)
+        trap_list = [trap_control]
+        cc_level.add_field(cc_classes.CCTrapControlsField(trap_list))
 
-        clone_control_b_coord = cc_classes.CCCoordinate(cc_cloning_machine_controls_field["bx"], cc_cloning_machine_controls_field["by"])
-        clone_control_t_coord = cc_classes.CCCoordinate(cc_cloning_machine_controls_field["tx"], cc_cloning_machine_controls_field["ty"])
-        clone_control = cc_classes.CCCloningMachineControl(clone_control_b_coord, clone_control_t_coord)
-        cc_level.add_field(cc_classes.CCCloningMachineControlsField(clone_control))
-        
+        clone_control_b_coord = cc_classes.CCCoordinate(cc_cloning_machine_controls_field["bx"],
+                                                        cc_cloning_machine_controls_field["by"])
+        clone_control_t_coord = cc_classes.CCCoordinate(cc_cloning_machine_controls_field["tx"],
+                                                        cc_cloning_machine_controls_field["ty"])
+        clone_control = cc_classes.CCCloningMachineControl(clone_control_b_coord.x, clone_control_b_coord.y,
+                                                           clone_control_t_coord.x, clone_control_t_coord.y)
+        clone_list = [clone_control]
+        cc_level.add_field(cc_classes.CCCloningMachineControlsField(clone_list))
+
         cc_level_pack.add_level(cc_level)
 
     return cc_level_pack
 
-input_json_file = "data/cc_level_pack.json"
+
+input_json_file = "data/ichhaya_cc1.json"
 
 with open(input_json_file, "r") as reader:
-        game_json_data = json.load(reader)
+    game_json_data = json.load(reader)
 
-cc_dat_utils.write_cc_level_pack_to_dat(cc_dat_utils.write_level_to_dat(json_to_cclevelpack(game_json_data), reader), "cc_test.dat")
+cc_level_pack_obj = json_to_cclevelpack(game_json_data)
+
+cc_dat_utils.write_cc_level_pack_to_dat(cc_level_pack_obj, "ichhaya_cc1.dat")
